085d0eb892b1ef4d283f1d49143a3f972b6a07f2
Urmila <urmila.mathew@gmail.com>
2021-04-20 16:55:28 +0100

    plots over london - added TODOs

--- src/components/Map3/MapboxGLMap.js
+++ src/components/Map3/MapboxGLMap.js
@@ -12,6 +12,10 @@ import mapboxgl, { Layer, Feature } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 // import ReactMapGL, { Layer } from 'react-map-gl';
 // import SnotelSites from './Icc_snotel_sites.json';
+/*TODO - Circle Radius should increase /decrease according to data values */
+/*TODO - Show a second data set as a layer or different source */
+/*TODO - Use UbiCov data not earthquake */
+
 
 
 
@@ -31,7 +35,7 @@ const MapboxGLMap = () => {
         "type": "FeatureCollection",
         "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
         "features": [
-            { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -151.5129, 63.1016, 0.0 ] } },
+            { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -151.5129, 63.1016, 300.0 ] } },
             { "type": "Feature", "properties": { "id": "ak16994519", "mag": 1.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -150.4048, 63.1224, 105.5 ] } }
 
         ]
@@ -50,25 +54,30 @@ const MapboxGLMap = () => {
             });
 
             map.on("load", () => {
-   /*             map.addSource('earthquakes', {
-                    type: 'geojson',
-                    data:{data},
-                        //'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
-                    cluster: true,
-                    clusterMaxZoom: 14, // Max zoom to cluster points on
-                    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-                });*/
+
+
+                // DATA SET 1 DEMO
+
                 map.addSource('earthquakes', {
                     type: 'geojson',
                     data: {
                         "type": "FeatureCollection",
+                        "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                         "features": [
-                            { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 0.0 ] } },
-                            { "type": "Feature", "properties": { "id": "ak16994519", "mag": 1.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 105.5 ] } }
+                            { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 1.8 ] } },
+                            { "type": "Feature", "properties": { "id": "ak16994521", "mag": 5.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 5.8 ] } },
+                            { "type": "Feature", "properties": { "id": "ak16994519", "mag": 0.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 1.5 ] }},
+                            { "type": "Feature", "properties": { "id": "ak16994519", "mag": 3.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 1005.5 ] }
+
+                            }
 
                         ]
-                    }
+                    },
+                    cluster: true,
+                    clusterMaxZoom: 14, // Max zoom to cluster points on
+                    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
                 });
+
 
                 map.addLayer({
                     id: 'clusters',
@@ -121,10 +130,83 @@ const MapboxGLMap = () => {
                     }
                 });
 
-                setMap(map);
-                map.resize();
-            });
-        };
+
+        // DATA SET 2 DEMO
+
+        map.addSource('ubicov', {
+            type: 'geojson',
+            data: {
+                "type": "FeatureCollection",
+                "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                "features": [
+                    { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 1.8 ] } },
+                    { "type": "Feature", "properties": { "id": "ak16994521", "mag": 5.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 5.8 ] } },
+                    { "type": "Feature", "properties": { "id": "ak16994519", "mag": 0.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 1.5 ] }},
+                    { "type": "Feature", "properties": { "id": "ak16994519", "mag": 3.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 1005.5 ] }
+
+                    }
+
+                ]
+            },
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom to cluster points on
+            clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+        });
+
+        map.addLayer({
+            id: 'clusters',
+            type: 'circle',
+            source: 'ubicov',
+            filter: ['has', 'point_count'],
+            paint: {
+                'circle-color': [
+                    'step',
+                    ['get', 'point_count'],
+                    '#51bbd6',
+                    100,
+                    '#f1f075',
+                    750,
+                    '#f28cb1'
+                ],
+                'circle-radius': [
+                    'step',
+                    ['get', 'point_count'],
+                    20, 100,
+                    30,
+                    750,
+                    40
+                ]
+            }
+        });
+
+        map.addLayer({
+            id: 'cluster-count',
+            type: 'symbol',
+            source: 'ubicov',
+            filter: ['has', 'point_count'],
+            layout: {
+                'text-field': '{point_count_abbreviated}',
+                'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+                'text-size': 12
+            }
+        });
+
+        map.addLayer({
+            id: 'unclustered-point',
+            type: 'circle',
+            source: 'ubicov',
+            filter: ['!', ['has', 'point_count']],
+            paint: {
+                'circle-color': '#f1f075',
+                'circle-radius': 4,
+                'circle-stroke-width': 1,
+                'circle-stroke-color': '#fff'
+            }
+        });
+        setMap(map);
+        map.resize();
+    });
+};
 
         if (!map) initializeMap({ setMap, mapContainer });
     }, [map]);
