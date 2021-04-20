edff9979ce998903a8539681ede17356fb7cca2a
Urmila <urmila.mathew@gmail.com>
2021-04-20 23:43:20 +0100

    added layer to show circles according to maginitude

--- src/components/Map3/MapboxGLMap.js
+++ src/components/Map3/MapboxGLMap.js
@@ -15,6 +15,8 @@ import "mapbox-gl/dist/mapbox-gl.css";
 /*TODO - Circle Radius should increase /decrease according to data values */
 /*TODO - Show a second data set as a layer or different source */
 /*TODO - Use UbiCov data not earthquake */
+/*TODO - Show hover tooltip with data -kepler style */
+
 
 
 
@@ -64,22 +66,74 @@ const MapboxGLMap = () => {
                         "type": "FeatureCollection",
                         "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                         "features": [
-                            { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 1.8 ] } },
-                            { "type": "Feature", "properties": { "id": "ak16994521", "mag": 5.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 5.8 ] } },
-                            { "type": "Feature", "properties": { "id": "ak16994519", "mag": 0.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 1.5 ] }},
-                            { "type": "Feature", "properties": { "id": "ak16994519", "mag": 3.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 1005.5 ] }
+                            { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.8, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 1.8 ] } },
+                            { "type": "Feature", "properties": { "id": "ak16994521", "mag": 5.8, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.011583929705352602, 51.463692453353815, 5.8 ] } },
+                            { "type": "Feature", "properties": { "id": "ak16994519", "mag": 4.0, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 1.5 ] }},
+                            { "type": "Feature", "properties": { "id": "ak16994519", "mag": 3.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.018132985217334863, 51.57993972933622, 1005.5 ] }
 
                             }
 
                         ]
-                    },
+                    }/*,
                     cluster: true,
                     clusterMaxZoom: 14, // Max zoom to cluster points on
-                    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+                    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)*/
                 });
 
+                map.addLayer(
+                    {
+                        'id': 'earthquakes-point',
+                        'type': 'circle',
+                        'source': 'earthquakes',
+                        'minzoom': 7,
+                        'paint': {
+// Size circle radius by earthquake magnitude and zoom level
+                            'circle-radius': [
+                                'interpolate',
+                                ['linear'],
+                                ['zoom'],
+                                7,
+                                ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
+                                16,
+                                ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
+                            ],
+// Color circle by earthquake magnitude
+                            'circle-color': [
+                                'interpolate',
+                                ['linear'],
+                                ['get', 'mag'],
+                                1,
+                                'rgb(103,169,207)',
+                                2,
+                                'rgb(103,169,207)',
+                                3,
+                                'rgb(103,169,207)',
+                                4,
+                                'rgb(103,169,207)',
+                                5,
+                                'rgb(103,169,207)',
+                                6,
+                                'rgb(103,169,207)',
+                            ],
+                            'circle-stroke-color': 'white',
+                            'circle-stroke-width': 1,
+// Transition from heatmap to circle layer by zoom level
+                            'circle-opacity': [
+                                'interpolate',
+                                ['linear'],
+                                ['zoom'],
+                                7,
+                                0,
+                                8,
+                                1
+                            ]
+                        }
+                    },
+                    'waterway-label'
+                );
 
-                map.addLayer({
+
+              /*  map.addLayer({
                     id: 'clusters',
                     type: 'circle',
                     source: 'earthquakes',
@@ -129,80 +183,9 @@ const MapboxGLMap = () => {
                         'circle-stroke-color': '#fff'
                     }
                 });
+*/
 
 
-        // DATA SET 2 DEMO
-
-        map.addSource('ubicov', {
-            type: 'geojson',
-            data: {
-                "type": "FeatureCollection",
-                "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-                "features": [
-                    { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 1.8 ] } },
-                    { "type": "Feature", "properties": { "id": "ak16994521", "mag": 5.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.30174587349631565, 51.552182823098406, 5.8 ] } },
-                    { "type": "Feature", "properties": { "id": "ak16994519", "mag": 0.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 1.5 ] }},
-                    { "type": "Feature", "properties": { "id": "ak16994519", "mag": 3.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [ -0.030458421517778334, 51.388296156862964, 1005.5 ] }
-
-                    }
-
-                ]
-            },
-            cluster: true,
-            clusterMaxZoom: 14, // Max zoom to cluster points on
-            clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-        });
-
-        map.addLayer({
-            id: 'clusters',
-            type: 'circle',
-            source: 'ubicov',
-            filter: ['has', 'point_count'],
-            paint: {
-                'circle-color': [
-                    'step',
-                    ['get', 'point_count'],
-                    '#51bbd6',
-                    100,
-                    '#f1f075',
-                    750,
-                    '#f28cb1'
-                ],
-                'circle-radius': [
-                    'step',
-                    ['get', 'point_count'],
-                    20, 100,
-                    30,
-                    750,
-                    40
-                ]
-            }
-        });
-
-        map.addLayer({
-            id: 'cluster-count',
-            type: 'symbol',
-            source: 'ubicov',
-            filter: ['has', 'point_count'],
-            layout: {
-                'text-field': '{point_count_abbreviated}',
-                'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-                'text-size': 12
-            }
-        });
-
-        map.addLayer({
-            id: 'unclustered-point',
-            type: 'circle',
-            source: 'ubicov',
-            filter: ['!', ['has', 'point_count']],
-            paint: {
-                'circle-color': '#f1f075',
-                'circle-radius': 4,
-                'circle-stroke-width': 1,
-                'circle-stroke-color': '#fff'
-            }
-        });
         setMap(map);
         map.resize();
     });
