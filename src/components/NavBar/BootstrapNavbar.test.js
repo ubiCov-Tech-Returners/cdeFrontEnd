d085372b577080f2b87508b277bf514882d1207a
Urmila <urmila.mathew@gmail.com>
2021-04-28 10:42:07 +0100

    added test to check dropdown links render onclick

--- src/components/NavBar/BootstrapNavbar.test.js
+++ src/components/NavBar/BootstrapNavbar.test.js
@@ -1,9 +1,17 @@
 
 import { render, screen } from '@testing-library/react';
 import BootstrapNavbar from './BootstrapNavbar';
+import userEvent from "@testing-library/user-event";
 
 test('renders Nav Bar correctly', () => {
     render(<BootstrapNavbar/>);
     const linkElement = screen.getByText(/Questions/i);
     expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Nav Bar dropdown when Questions link is clicked', () => {
+    render(<BootstrapNavbar/>);
+    const dropdownLink = screen.getByText('Questions');
+    userEvent.click(dropdownLink);
+    expect(screen.getByText('income',{exact:false})).toBeInTheDocument();
 });
